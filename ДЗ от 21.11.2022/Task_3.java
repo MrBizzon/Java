1cb4627d80078a523ff36ac66783b1e91b700f5f
MrBizzon <info.proect@mail.ru>
2022-11-22 17:42:57 +0300

    gitignore

---
+++ ДЗ от 21.11.2022/Task_3.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Task_3 {
+	public static void main(String[] args) {
+
+		double firstNumber, secondNumber;
+		double resultOperation;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите первое число: ");
+		firstNumber = scan.nextDouble();
+
+		System.out.print("Введите второе число: ");
+		secondNumber = scan.nextDouble();
+
+		System.out.print("Введите знак операции (+, -, /, *, ^, %): ");
+
+		char operationSign = scan.next().charAt(0);
+
+		switch (operationSign) {
+			case '+':
+				resultOperation = firstNumber + secondNumber;
+				break;
+
+			case '-':
+				resultOperation = firstNumber - secondNumber;
+				break;
+
+			case '*':
+				resultOperation = firstNumber * secondNumber;
+				break;
+
+			case '/':
+				resultOperation = firstNumber / secondNumber;
+				break;
+
+			case '%':
+				resultOperation = firstNumber % secondNumber;
+				break;
+
+			case '^':
+				resultOperation = Math.pow(firstNumber, secondNumber);
+				break;
+
+			case 'Q':
+				System.exit(0);
+
+			default:
+				System.out.printf("Вы ввели неверный знак операции");
+				return;
+		}
+		System.out.println(firstNumber + " " + operationSign + " " + secondNumber + " = " + resultOperation);
+	}
+}
