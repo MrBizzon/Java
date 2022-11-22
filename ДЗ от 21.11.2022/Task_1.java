c4a83d41e03c2e139d6f3ebcf178d4a8108bc457
MrBizzon <info.proect@mail.ru>
2022-11-22 16:42:45 +0300

    First & second task

---
+++ ДЗ от 21.11.2022/Task_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = scan.nextInt();
+        int multi = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " * ");
+            multi *= i;
+        }
+
+        System.out.print(" = " + multi);
+    }
+}
