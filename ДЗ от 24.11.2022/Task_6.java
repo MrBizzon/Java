7cd68c3b5970aa30a0ae0b0861239717be187164
MrBizzon <info.proect@mail.ru>
2022-11-25 19:04:18 +0300

    Small changes

--- ДЗ от 24.11.2022/Task_6.java
+++ ДЗ от 24.11.2022/Task_6.java
@@ -2,8 +2,6 @@
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
