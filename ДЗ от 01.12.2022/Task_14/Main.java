c6158282a72637cfbbcd7dfbbd7d6389e5bf7f70
MrBizzon <info.proect@mail.ru>
2022-12-03 13:48:10 +0300

    Added calculator

---
+++ ДЗ от 01.12.2022/Task_14/Main.java
@@ -0,0 +1,10 @@
+// В калькулятор добавьте возможность отменить последнюю операцию.
+
+package Task_14;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator w = new Calculator();
+    }
+
+}
